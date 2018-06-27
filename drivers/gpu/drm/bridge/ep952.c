45a4d8bd171a7425451fca4de05bba89df0ce4d7
Maxime Ripard <maxime.ripard@bootlin.com>
2018-06-27 19:03:28 +0200

    wip
    Signed-off-by: Maxime Ripard <maxime.ripard@bootlin.com>

--- drivers/gpu/drm/bridge/ep952.c
+++ drivers/gpu/drm/bridge/ep952.c
@@ -18,25 +18,36 @@
 
 #include <linux/gpio/consumer.h>
 
-#define EP952_CTL1_REG		0x08
-#define EP952_CTL1_EDGE			BIT(1)
+#define EP952_TXPHY_CTL0_REG		0x00
+#define EP952_TXPHY_CTL0_TERM_EN		BIT(7)
 
-#define EP952_CS_CTL_REG	0x0c
-#define EP952_CS_CTL_VMUTE		BIT(3)
+#define EP952_TXPHY_CTL1_REG		0x01
 
-#define EP952_CTL4_REG		0x0e
-#define EP952_CTL4_HDMI			BIT(0)
+#define EP952_CTL1_REG			0x08
+#define EP952_CTL1_INT_OD			BIT(6)
+#define EP952_CTL1_EDGE				BIT(1)
 
-#define EP952_IIS_CTL_REG	0x3f
-#define EP952_IIS_CTL_AVI_EN		BIT(6)
+#define EP952_CS_CTL_REG		0x0c
+#define EP952_CS_CTL_V_MUTE			BIT(3)
+#define EP952_CS_CTL_A_MUTE			BIT(2)
+
+#define EP952_CTL4_REG			0x0e
+#define EP952_CTL4_HDMI				BIT(0)
+
+#define EP952_IIS_CTL_REG		0x3f
+#define EP952_IIS_CTL_AVI_EN			BIT(6)
+
+#define EP952_AVI_INFOFRAME_REG(reg)	(0x66 + (reg))
 
 struct ep952 {
-	struct drm_bridge	bridge;
-	struct drm_connector	connector;
-	struct i2c_client	*client;
-	struct i2c_adapter	*ddc;
-	bool			hdmi_mode;
-	struct gpio_desc	*reset;
+	struct drm_bridge		bridge;
+	struct drm_connector		connector;
+	struct i2c_client		*client;
+	struct i2c_adapter		*ddc;
+	struct gpio_desc		*reset;
+
+	const struct drm_display_mode	*current_mode;
+	bool				hdmi_mode;
 };
 
 static inline struct ep952 *bridge_to_ep952(struct drm_bridge *bridge)
@@ -101,7 +112,7 @@ static const struct drm_connector_helper_funcs ep952_connector_helper_funcs = {
 static enum drm_connector_status ep952_detect(struct drm_connector *connector,
 					      bool force)
 {
-	return connector_status_disconnected;
+	return connector_status_connected;
 }
 
 static const struct drm_connector_funcs ep952_connector_funcs = {
@@ -124,9 +135,9 @@ static void ep952_hw_reset(struct ep952 *ep952)
 	msleep(10);
 }
 
-static void ep952_write_infoframes(struct ep952 *ep952,
-				   const struct drm_display_mode *mode)
+static void ep952_write_infoframes(struct ep952 *ep952)
 {
+	const struct drm_display_mode *mode = ep952->current_mode;
 	struct hdmi_avi_infoframe frame;
 	u8 avi_buf[HDMI_INFOFRAME_SIZE(AVI)];
 	int i, ret;
@@ -143,10 +154,13 @@ static void ep952_write_infoframes(struct ep952 *ep952,
 		return;
 	}
 
-	for (i = HDMI_INFOFRAME_HEADER_SIZE; i < HDMI_INFOFRAME_SIZE(AVI); i++)
-		ep952_write_reg(ep952,
-				EP952_AVI_REG(i - HDMI_INFOFRAME_HEADER_SIZE),
-				avi_buf[i]);
+	for (i = HDMI_INFOFRAME_HEADER_SIZE; i < HDMI_INFOFRAME_SIZE(AVI); i++) {
+		u8 reg = EP952_AVI_INFOFRAME_REG(i - HDMI_INFOFRAME_HEADER_SIZE);
+
+		ep952_write_reg(ep952, reg, avi_buf[i]);
+	}
+
+	/* TODO: ADO Infoframes */
 }
 
 static void ep952_enable(struct drm_bridge *bridge)
@@ -156,20 +170,37 @@ static void ep952_enable(struct drm_bridge *bridge)
 	ep952_hw_reset(ep952);
 
 	ep952_set_bit(ep952, EP952_TXPHY_CTL0_REG, EP952_TXPHY_CTL0_TERM_EN);
-	ep952_set_bit(ep952, EP952_CS_CTL_REG, EP952_CS_CTL_VMUTE);
-
-	if (ep952->hdmi_mode) {
+	ep952_set_bit(ep952, EP952_CS_CTL_REG, EP952_CS_CTL_A_MUTE);
+	ep952_set_bit(ep952, 0x0d, 0x40);
+	ep952_set_bit(ep952, EP952_CS_CTL_REG, EP952_CS_CTL_V_MUTE);
+	ep952_set_bit(ep952, 0x40, 0x08);
+	ep952_set_bit(ep952, EP952_CTL1_REG, EP952_CTL1_INT_OD);
+	
+	if (ep952->hdmi_mode)
 		ep952_write_infoframes(ep952);
+
+	/* TODO: Write Channel Status Register to 0 (5 0 bytes, to 0x7b) */
+
+	if (ep952->hdmi_mode)
 		ep952_write_reg(ep952, EP952_CTL4_REG, EP952_CTL4_HDMI);
-	}
 
 	if (display->flags & DISPLAY_FLAGS_PIXDATA_POSEDGE)
 		ep952_set_bit(ep952, EP952_CTL1_REG, EP952_CTL1_EDGE);
 	else
 		ep952_clr_bit(ep952, EP952_CTL1_REG, EP952_CTL1_EDGE);
 
-	ep952_set_bit(ep952, EP952_IIS_CTL_REG, EP952_IIS_CTL_AVI_EN);
+	ep952_set_bit(ep952, 0x33, BIT(2));
+	ep952_set_bit(ep952, 0x0c, BIT(4));
+	ep952_set_bit(ep952, 0x0c, BIT(5));
+	ep952_set_bit(ep952, EP952_IIS_CTL_REG, EP952_IIS_CTL_AVI_EN | EP952_IIS_CTL_GC_EN);
+
 	ep952_clr_bit(ep952, EP952_CS_CTL_REG, EP952_CS_CTL_VMUTE);
+	ep952_clr_bit(ep952, 0x0d, 0x40);
+	ep952_clr_bit(ep952, EP952_CS_CTL_REG, EP952_CS_CTL_AMUTE);
+
+	ep952_set_bit(ep952, 0x3f, 0x20);
+	ep952_set_bit(ep952, 0x3f, 0x08);
+	ep952_set_bit(ep952, 0x08, 0x01);
 }
 
 static void ep952_disable(struct drm_bridge *bridge)
@@ -197,6 +228,11 @@ static int ep952_attach(struct drm_bridge *bridge)
 
 	return 0;
 }
+
+static void ep952_bridge_mode_set(struct drm_bridge *bridge,
+				  struct drm_display_mode *mode,
+				  struct drm_display_mode *adj)
+{
 
 static const struct drm_bridge_funcs ep952_bridge_funcs = {
 	.attach		= ep952_attach,
