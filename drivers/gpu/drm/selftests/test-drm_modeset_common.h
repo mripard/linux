9cc497d117aa452525767edc6bb03cd4c60855da
Maxime Ripard <maxime.ripard@bootlin.com>
2019-03-04 15:33:17 +0100

    drm/fourcc: Remove old DRM format API
    Now that all the clients of the old drm_format* API have been converted to
    the generic one, let's remove it.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@bootlin.com>

--- drivers/gpu/drm/selftests/test-drm_modeset_common.h
+++ drivers/gpu/drm/selftests/test-drm_modeset_common.h
@@ -14,9 +14,6 @@
 #define FAIL_ON(x) FAIL((x), "%s", "FAIL_ON(" __stringify(x) ")\n")
 
 int igt_check_plane_state(void *ignored);
-int igt_check_drm_format_block_width(void *ignored);
-int igt_check_drm_format_block_height(void *ignored);
-int igt_check_drm_format_min_pitch(void *ignored);
 int igt_check_drm_framebuffer_create(void *ignored);
 int igt_damage_iter_no_damage(void *ignored);
 int igt_damage_iter_no_damage_fractional_src(void *ignored);
